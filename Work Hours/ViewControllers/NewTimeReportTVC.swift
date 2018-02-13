305b9160a72c4f64379fa148eafa8f4110ed08c4
JacobAhlberg <jacahlberg@hotmail.com>
2018-02-13 16:03:41 +0100

    Doesn't remove userDefaults when discarding a new time report

--- Work Hours/ViewControllers/NewTimeReportTVC.swift
+++ Work Hours/ViewControllers/NewTimeReportTVC.swift
@@ -87,7 +87,6 @@ class NewTimeReportTVC: UITableViewController, CLLocationManagerDelegate, MKMapV
     @IBAction func cancelBtnPressed(_ sender: Any) {
         let alertVC = UIAlertController(title: NSLocalizedString("Are you sure?", comment: "Are you sure?"), message: NSLocalizedString("Do you want to discard current report?", comment: "Do you want to discard current report?"), preferredStyle: .alert)
         let discardAction = UIAlertAction(title: NSLocalizedString("Discard", comment: "Discard"), style: .destructive) { (alert) in
-            clearUserDefaults()
             self.navigationController?.popViewController(animated: true)
         }
         let cancelAction = UIAlertAction(title: NSLocalizedString("Cancel", comment: "Cancel newTimeReport"), style: .cancel, handler: nil)
