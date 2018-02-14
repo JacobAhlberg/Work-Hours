7f0fd2cb5620999592c89911c11a0e2595986070
JacobAhlberg <jacahlberg@hotmail.com>
2018-02-14 15:13:52 +0100

    Removed user location annotation and added default annotation instead

--- Work Hours/ViewControllers/MapVC.swift
+++ Work Hours/ViewControllers/MapVC.swift
@@ -95,6 +95,9 @@ class MapVC: UIViewController, UISearchBarDelegate, CLLocationManagerDelegate, H
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.first {
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = location.coordinate
+            mapView.addAnnotation(annotation)
             let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
             let region = MKCoordinateRegion(center: location.coordinate, span: span)
             mapView.setRegion(region, animated: true)
