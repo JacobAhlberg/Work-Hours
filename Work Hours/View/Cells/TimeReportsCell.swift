7fb732e337df03d58201fd5021553411d092fe0f
Giovanni Palusa <giovanni@palusa.se>
2018-02-15 19:19:27 +0100

    started creating cells for timeReports

---
+++ Work Hours/View/Cells/TimeReportsCell.swift
@@ -0,0 +1,14 @@
+//
+//  TimeReportsCell.swift
+//  Work Hours
+//
+//  Created by Giovanni Palusa on 2018-02-15.
+//  Copyright © 2018 Jacob Ahlberg. All rights reserved.
+//
+
+import UIKit
+
+class TimeReportsCell: UITableViewCell {
+
+    
+}
