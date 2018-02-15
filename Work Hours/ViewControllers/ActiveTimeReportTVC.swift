ba8ab1e03d6cc90017cab8961b0d2ea6c94de002
JacobAhlberg <jacahlberg@hotmail.com>
2018-02-15 23:50:49 +0100

    Fixed image size in uicollectionView in activeVC

--- Work Hours/ViewControllers/ActiveTimeReportTVC.swift
+++ Work Hours/ViewControllers/ActiveTimeReportTVC.swift
@@ -96,4 +96,22 @@ class ActiveTimeReportTVC: UITableViewController, UICollectionViewDataSource, UI
         return imagesNames.count
     }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (collectionView.bounds.width / 3) - 2
+        let height = width
+        return CGSize(width: width, height: height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets.zero
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 3
+    }
+    
 }
